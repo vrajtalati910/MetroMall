3d5be852fedce30538382b04fbf234ea0b4c85f1
vraj talati <111729980+vrajtalati910@users.noreply.github.com>
2025-09-19 12:51:31 +0530

    Update app_string.dart

--- lib/core/constant/app_string.dart
+++ lib/core/constant/app_string.dart
@@ -4,7 +4,7 @@ class AppStrings {
   static const userPrefKey = 'user';
   static const printPrefKey = 'print';
 
-  static const baseUrl = 'https://tailormate-api-production.up.railway.app/api';
+  static const baseUrl = 'https://tailormate-api.metromall.co.in';
 
   // AUTH
   static const login = '$baseUrl/v1/login';
