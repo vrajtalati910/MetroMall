e2bf25ca1f9fdab54b58217bc5aef2d49123bcc7
vraj <vrajtalati910@gmail.com>
2025-09-19 13:07:23 +0530

    feat: web

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,12 +1,23 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:tailor_mate/injector/injector.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 Future<void> bootstrap(Widget builder) async {
   Zone.current.fork().runGuarded(() async {
+    // Remove # from the URL
+    setPathUrlStrategy();
+
+    if (!kIsWeb) {
+      HttpOverrides.global = MyHttpOverrides();
+    }
+
     WidgetsFlutterBinding.ensureInitialized();
     await configureDependencies();
 
@@ -21,10 +32,26 @@ Future<void> bootstrap(Widget builder) async {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    runApp(builder);
+    runApp(
+      kIsWeb
+          ? FlutterWebFrame(
+              maximumSize: const Size(414, 896),
+              enabled: true,
+              builder: (context) => builder,
+            )
+          : builder,
+    );
 
     FlutterError.onError = (details) {
       log(details.exceptionAsString(), stackTrace: details.stack);
     };
   });
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
 }
