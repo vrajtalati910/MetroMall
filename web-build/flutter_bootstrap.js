b4e12821b4a4717ba62a9adbd33df9b51078b991
vraj <vrajtalati910@gmail.com>
2025-09-20 13:14:19 +0530

    feat: changes

--- web-build/flutter_bootstrap.js
+++ web-build/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3472589380"
+    serviceWorkerVersion: "3167664431"
   }
 });
